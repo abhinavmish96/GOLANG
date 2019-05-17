67a38a16ff594d4b5565b1db3272a145d8e357e4
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-18 01:04:57 +0530

    <Prog 3>

--- variables.go
+++ variables.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	// var declares 1 or more variables.
 	var a = "initial"
 	fmt.Println(a)
@@ -22,5 +22,5 @@ func main() {
 
 	//The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "short" in this case.
 	f:= "short"
-	fmt.Print(f)
+	fmt.Println(f)
 }
