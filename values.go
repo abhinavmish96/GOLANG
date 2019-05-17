319646a31f9ed072c3961ed88c8a092ea68de5dd
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-18 01:03:52 +0530

    <Prog 3>

--- values.go
+++ values.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("go" + "lang");
 
-	fmt.Println("1+1 =", 1+1);
-	fmt.Println("7.0/3.0 =", 7.0/3.0);
+	//Strings, which can be added together with +.
+	fmt.Println("go" + "lang")
 
-	fmt.Println(true && false);
-	fmt.Println(true || false);
-	fmt.Println(!true);
+	//Integers and floats.
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//Booleans, with boolean operators as you’d expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
