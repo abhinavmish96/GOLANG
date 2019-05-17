279a6073202d4a1216ee70891d9d907cf2d5c96b
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-18 00:48:27 +0530

    <Prog 1>

--- helloworld.go
+++ helloworld.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-    fmt.Println("hello world")
+func  main(){
+	fmt.Println("Hello World!");
 }
--- values.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("klkm     "+"tbgbt")
-	fmt.Println(1+2)
-	fmt.Println(!false)
-}
