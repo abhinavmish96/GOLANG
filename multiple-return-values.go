af8603a5bbe3048bdf8d2bcb40b63f8dff82df89
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-27 12:13:05 +0530

    <multiple-return-values>

---
+++ multiple-return-values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//The (int, int) in this function signature shows that the function returns 2 ints
+
+	//Here we use the 2 different return values from the call with multiple assignment
+
+	//If you only want a subset of the returned values, use the blank identifier _
+
+}
+
+//Accepting a variable number of arguments is another nice feature of Go functions
