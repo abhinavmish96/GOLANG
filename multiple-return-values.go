016f53c96cc62d83f034f525696eaf8b36848e67
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-27 12:16:30 +0530

    <multiple-return-values>

--- multiple-return-values.go
+++ multiple-return-values.go
@@ -2,14 +2,21 @@ package main
 
 import "fmt"
 
+//The (int, int) in this function signature shows that the function returns 2 ints
+func vals() (int, int){
+	return 3, 7
+}
+
 func main(){
-
-	//The (int, int) in this function signature shows that the function returns 2 ints
-
+	
 	//Here we use the 2 different return values from the call with multiple assignment
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
 
 	//If you only want a subset of the returned values, use the blank identifier _
-
+	_, c := vals()
+	fmt.Println(c)
 }
 
 //Accepting a variable number of arguments is another nice feature of Go functions
