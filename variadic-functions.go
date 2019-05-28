41b9a43cc23aa146f4ee21d37958b0cf6481cc68
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-28 13:06:26 +0530

    <variadic-functions>

---
+++ variadic-functions.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Here’s a function that will take an arbitrary number of ints as arguments
+
+func main(){
+	//Variadic functions can be called in the usual way with individual arguments
+	
+	//If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this
+}
+
+//Another key aspect of functions in Go is their ability to form closures
