b8c15549dc7c2c83278d0764c3561afe72070bd9
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-29 13:00:24 +0530

    <closures>

--- closures.go
+++ closures.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 //This function intSeq returns another function, which we define anonymously in the body of intSeq. The returned function closes over the variable i to form a closure
-func intSeq(){
+func intSeq() func() int {
 	i := 0
 	return func() int {
 		i++
