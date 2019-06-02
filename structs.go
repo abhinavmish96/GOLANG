ca50ea164ab0be35ff0b97b53d98f762bb323e90
Abhinav Mishra <abhinavmish96@gmail.com>
2019-06-02 12:32:56 +0530

    <structs>

---
+++ structs.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//This person struct type has name and age fields
+type person struct {
+	name string
+	age int
+}
+
+func main() {
+	//This syntax creates a new struct
+
+	//You can name the fields when initializing a struct
+
+	//Omitted fields will be zero-valued
+
+	//An & prefix yields a pointer to the struct
+
+	//Access struct fields with a dot
+
+	//You can also use dots with struct pointers - the pointers are automatically dereferenced
+
+	//Structs are mutable
+
+}
