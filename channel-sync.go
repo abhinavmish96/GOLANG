d000d9deab2a04007d8cbe53d54593cf582d3697
Abhinav Mishra <abhinavmish96@gmail.com>
2019-06-23 15:23:20 +0530

    <channel-sync>

--- channel-sync.go
+++ channel-sync.go
@@ -3,6 +3,7 @@ package main
 //We can use channels to synchronize execution across goroutines. Here’s an example of using a blocking receive to wait for a goroutine to finish. When waiting for multiple goroutines to finish, you may prefer to use a WaitGroup.
 
 import "fmt"
+import "time"
 
 //This is the function we’ll run in a goroutine. The done channel will be used to notify another goroutine that this function’s work is done.
 func worker(done chan bool){
