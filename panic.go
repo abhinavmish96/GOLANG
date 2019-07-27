11ea9909664d9548d8ce728887c0a025444365d2
Abhinav Mishra <abhinavmish96@gmail.com>
2019-07-27 23:49:35 +0530

    <panic>

--- panic.go
+++ panic.go
@@ -1,6 +1,6 @@
 package main
 
-//A panic typically means something went unexpectedly wrong. Mostly we use it to fail fast on errors that shouldn’t occur during normal operation, or that we aren’t prepared to handle gracefully.
+//A panic typically means something went unexpectedly wrong. Mostly we use it to fail fast on errors that shouldn’t occur during normal operation, or that we aren’t prepared to handle gracefully
 
 import "os"
 
