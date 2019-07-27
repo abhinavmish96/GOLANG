9863cebf3ce36f3b02f6ff08e404ffff56b58f94
Abhinav Mishra <abhinavmish96@gmail.com>
2019-07-27 23:50:14 +0530

    <panic>

--- panic.go
+++ panic.go
@@ -5,10 +5,15 @@ package main
 import "os"
 
 func main() {
+	panic("a problem")
 
 	//We’ll use panic throughout this site to check for unexpected errors. This is the only program on the site designed to panic.
 
 	//A common use of panic is to abort if a function returns an error value that we don’t know how to (or want to) handle. Here’s an example of panicking if we get an unexpected error when creating a new file.
+	_, err := os.Create("/tmp/file")
+    if err != nil {
+        panic(err)
+    }
 }
 
 //Running this program will cause it to panic, print an error message and goroutine traces, and exit with a non-zero status.
