8978421119f61817ca5452142933a3c860a7d1b0
Abhinav Mishra <abhinavmish96@gmail.com>
2019-09-30 11:16:47 +0530

    <cmd-line-arg>

--- cmd-line-arg.go
+++ cmd-line-arg.go
@@ -26,3 +26,13 @@ func main() {
 	fmt.Println(argsWithoutProg)
 	fmt.Println(arg)
 }
+
+/*
+
+$ go build command-line-arguments.go
+$ ./command-line-arguments a b c d
+[./command-line-arguments a b c d]       
+[a b c d]
+c
+
+*/
