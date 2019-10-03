130f6cdae7e8811be3f7c016a56bbe651f1177c7
Abhinav Mishra <abhinavmish96@gmail.com>
2019-10-03 21:06:04 +0530

    <cmd-line-sub>

--- cmd-line-sub.go
+++ cmd-line-sub.go
@@ -55,3 +55,26 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+/*
+
+$ go build command-line-subcommands.go 
+
+$ ./command-line-subcommands foo -enable -name=joe a1 a2
+subcommand 'foo'
+  enable: true
+  name: joe
+  tail: [a1 a2]
+
+$ ./command-line-subcommands bar -level 8 a1
+subcommand 'bar'
+  level: 8
+  tail: [a1]
+
+$ ./command-line-subcommands bar -enable a1
+flag provided but not defined: -enable
+Usage of bar:
+  -level int
+        level
+
+*/
