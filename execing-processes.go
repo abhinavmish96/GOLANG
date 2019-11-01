83e37aba568654026ad37a05f6adab59c9a4dd15
Abhinav Mishra <abhinavmish96@gmail.com>
2019-11-02 01:18:14 +0530

    <execing-processes>

--- execing-processes.go
+++ execing-processes.go
@@ -48,3 +48,11 @@ func main() {
 		panic(execErr)
 	}
 }
+
+/*
+$ go run execing-processes.go
+total 16
+drwxr-xr-x  4 mark 136B Oct 3 16:29 .
+drwxr-xr-x 91 mark 3.0K Oct 3 12:50 ..
+-rw-r--r--  1 mark 1.3K Oct 3 16:28 execing-processes.go
+*/
