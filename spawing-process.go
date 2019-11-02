1b5a0d73ff42bee1265913663afc016e7947af83
Abhinav Mishra <abhinavmish96@gmail.com>
2019-11-03 00:08:01 +0530

    <spawning-process>

--- spawing-process.go
+++ spawing-process.go
@@ -71,3 +71,15 @@ func main() {
 	fmt.Println("> ls -a -l -h")
 	fmt.Println(string(lsOut))
 }
+
+/*
+$ go run spawning-processes.go 
+> date
+Wed Oct 10 09:53:11 PDT 2012
+> grep hello
+hello grep
+> ls -a -l -h
+drwxr-xr-x  4 mark 136B Oct 3 16:29 .
+drwxr-xr-x 91 mark 3.0K Oct 3 12:50 ..
+-rw-r--r--  1 mark 1.3K Oct 3 16:28 spawning-processes.go
+*/
